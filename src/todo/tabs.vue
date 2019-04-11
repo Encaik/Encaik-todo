9337d1f95136f760d8e11cea46ef0db6a0edc5a6
st843391622 <843391622@qq.com>
2019-04-11 15:26:46 +0800

    update 4.11

--- src/todo/tabs.vue
+++ src/todo/tabs.vue
@@ -1,42 +1,54 @@
+    
 <template>
-    <div class="helper">
-        <span class="left">2 items left</span>
-        <span class="tabs">
-            <span
-                v-for="state in states"
-                :key="state"
-                :class="[state, filter === state ? 'active' : '']"
-                @click="toggleFilter(state)"
-            >
-                {{state}}
-            </span>
-        </span>
-        <span class="clear" @click="clearAllCompleted">clearAllCompleted</span>
-    </div>
+  <div class="helper">
+    <span class="left">{{unFinishedTodoLength}} items left</span>
+    <span class="tabs">
+      <span
+        v-for="state in states"
+        :key="state"
+        :class="[state, filter === state ? 'actived' : '']"
+        @click="toggleFilter(state)"
+      >{{state}}</span>
+    </span>
+    <span class="clear" @click="clearAllCompleted">Clear Completed</span>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        filter: {
-            type:String,
-            required: true,
-        }
+  props: {
+    filter: {
+      type: String,
+      required: true
     },
-    data() {
-        return {
-            states: ['all', 'active', 'completed']
-        }
-    },
-    methods: {
-        clearAllCompleted(){},
-        toggleFilter(){},
+    todos: {
+      type: Array,
+      required: true
     }
-}
+  },
+  data() {
+    return {
+      states: ["all", "active", "completed"]
+    };
+  },
+  computed: {
+    unFinishedTodoLength() {
+      return this.todos.filter(todo => !todo.completed).length;
+    }
+  },
+  methods: {
+    clearAllCompleted() {
+      this.$emit("clearAllCompleted");
+    },
+    toggleFilter(state) {
+      this.$emit("toggle", state);
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-.helper{
+.helper {
   font-weight: 100;
   display: flex;
   justify-content: space-between;
@@ -45,21 +57,24 @@ export default {
   background-color: #fff;
   font-size: 14px;
 }
-.left, .clear, .tabs{
+.left,
+.clear,
+.tabs {
   padding: 0 10px;
   box-sizing: border-box;
 }
-.left, .clear{
+.left,
+.clear {
   width: 150px;
 }
-.left{
+.left {
   text-align: left;
 }
-.clear{
+.clear {
   text-align: right;
   cursor: pointer;
 }
-.tabs{
+.tabs {
   width: 200px;
   display: flex;
   justify-content: space-around;
@@ -67,9 +82,9 @@ export default {
     display: inline-block;
     padding: 0 10px;
     cursor: pointer;
-    border: 1px solid rgba(175,47,47,0);
-    &.actived{
-      border-color: rgba(175,47,47,0.4);
+    border: 1px solid rgba(175, 47, 47, 0);
+    &.actived {
+      border-color: rgba(175, 47, 47, 0.4);
       border-radius: 5px;
     }
   }
